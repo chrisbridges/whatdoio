9272be14851195a17ebdbd427508def91a9abb96
chris.bridges <christopher_bridges@icloud.com>
2018-03-25 16:02:20 -0700

    added travis and heroku ci

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+app.use(morgan('common'));
 
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
+
+module.exports = {app};
---
+++ test/test-server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app} = require('../server');
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('make sure root serves static html', function () {
+  it('should serve home page', function () {
+    return chai
+    .request(app)
+    .get('/')
+    .then(res => {
+      expect(res).to.have.status(200);
+    });
+  });
+});
