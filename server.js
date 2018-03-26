4288686882481d403c64e799f132186e9a1190db
chris.bridges <christopher_bridges@icloud.com>
2018-03-25 17:04:44 -0700

    tests should auto end now

--- server.js
+++ server.js
@@ -6,4 +6,10 @@ app.use(morgan('common'));
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
 
+if (require.main === module) {
+  app.listen(process.env.PORT || 8080, function () {
+    console.info(`App listening on ${this.address().port}`);
+  });
+}
+
 module.exports = {app};
