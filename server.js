8a6e6ae836106482d04299299ec4526f53949294
chris.bridges <christopher_bridges@icloud.com>
2018-03-27 13:19:29 -0700

    added run and close server funcs

--- server.js
+++ server.js
@@ -18,6 +18,41 @@ app.use('/signup', signupRouter);
 app.use('/login', loginRouter);
 app.use('/user', userRouter);
 
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
 if (require.main === module) {
   app.listen(process.env.PORT || 8080, function () {
     console.info(`App listening on ${this.address().port}`);
