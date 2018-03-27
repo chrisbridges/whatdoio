09b7e305fc01390b0f1075277fd024b792b0abb5
Chris Bridges <christopher_bridges@icloud.com>
2018-03-26 18:30:44 -0700

    started adding mock data

---
+++ app.js
@@ -0,0 +1,13 @@
+const MOCK_USER_BILL_DATA = {
+  "bills": [
+    {
+      id: 11111,
+      from: ['Connor'],
+      for: ['Chris'],
+      recurring: false,
+      interval: null,
+      title: 'KBBQ',
+      amount: 100
+    }
+  ]
+}
