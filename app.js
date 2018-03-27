7c993969dfd495c95f78812ea148c65fba8562f6
Chris Bridges <christopher_bridges@icloud.com>
2018-03-27 12:41:32 -0700

    api mock data in place and working on user-page load

--- app.js
+++ app.js
@@ -14,7 +14,14 @@ const MOCK_USER_BILL_DATA = {
       from: ['Chris'],
       for: ['Cas, Mom, Conner, Justin'],
       recurring: true,
-      interval: null,
+      interval: {
+        daily: null,
+        weekly: null,
+        monthly: {
+          dueDate: 6
+        },
+        yearly: null
+      },
       title: 'Phone Bill',
       amount: 150
     },
@@ -23,9 +30,55 @@ const MOCK_USER_BILL_DATA = {
       from: ['Josh'],
       for: ['Chris'],
       recurring: true,
-      interval: null,
+      interval: {
+        daily: null,
+        weekly: null,
+        monthly: {
+          dueDate: 10
+        },
+        yearly: null
+      },
       title: 'Rent',
       amount: 800
+    },
+    {
+      id: 44444,
+      from: ['Saachi'],
+      for: ['Chris'],
+      recurring: true,
+      interval: {
+        daily: null,
+        weekly: {
+          dueDay: 'Wednesday'
+        },
+        monthly: null,
+        yearly: null
+      },
+      title: 'Something',
+      amount: 5
     }
   ]
+};
+
+function getUserBills(callbackFn) {
+  setTimeout(function(){ callbackFn(MOCK_USER_BILL_DATA)}, 100);
 }
+
+// this function stays the same when we connect
+// to real API later
+function displayUserBills(data) {
+  for (index in data.bills) {
+     $('body').append(
+      '<p>' + data.bills[index].title + '</p>');
+  }
+}
+
+// this function can stay the same even when we
+// are connecting to real API
+function getAndDisplayUserBills() {
+  getUserBills(displayUserBills);
+}
+
+$(function() {
+  getAndDisplayUserBills();
+})
